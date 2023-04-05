28ed6ce873f62385d2123038a8be8019ad567835
Matthew J. Moldovan <matthewmoldovan@outlook.com>
2023-04-05 16:06:02 -0600

    CarAndDriver

---
+++ src/com/mattmoldovan/CarAndDriver/Car.java
@@ -0,0 +1,23 @@
+package com.mattmoldovan.CarAndDriver;
+
+public class Car {
+	
+	private int gas;
+	
+	public Car() {
+		this.gas = 10;
+	}
+
+	public int getGas() {
+		return gas;
+	}
+
+	public void setGas(int gas) {
+		this.gas = gas;
+	}
+	
+	public void display() {
+		System.out.println("Gas: " + this.gas);
+	}
+
+}
---
+++ src/com/mattmoldovan/CarAndDriver/CarAndDriverTest.java
@@ -0,0 +1,21 @@
+package com.mattmoldovan.CarAndDriver;
+
+public class CarAndDriverTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Driver testDriver = new Driver();
+		
+		testDriver.drive();
+		testDriver.drive();
+		testDriver.drive();
+		testDriver.drive();
+		testDriver.useBooster();
+		testDriver.refuel();
+		testDriver.refuel();
+		testDriver.refuel();
+
+	}
+
+}
